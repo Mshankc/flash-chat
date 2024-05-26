33ff2ad70a0b5db43380e03b6ebe5d1be2ebf78a
mshan <mshankc@gmail.com>
2024-05-26 23:23:55 +0530

    .evm created

---
+++ lib/confidentials.dart
@@ -0,0 +1,6 @@
+const String kApiKey = 'AIzaSyCrTC89HNPhUAKDR_3M1Z3-7b0IeS8esCE';
+const String kAppId = '1:218797752184:android:b8fbf9617061a58072d28d';
+const String kMessagingSenderId = '218797752184';
+const String kProjectId = 'flash-chat-b4564';
+const String kStorageBucket = 'flash-chat-b4564.appspot.com';
+const String kIosBundleId = 'com.kcltd.example.flashChat';
